65cb6f202a9cca6a31e101b7fd03ae857b827945
pavel <pavel@example.com>
2020-11-08 20:45:04 +0300

    added parallax, router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import s from './App.module.scss';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+import { HomePage } from './pages/Home';
+import { PokedexPage } from './pages/Pokedex';
 
 export const App = () => {
   return (
     <div className={s.app}>
-      <Header />
-      <div>content</div>
-      <Footer />
+      <BrowserRouter>
+        <Route path="/home" component={HomePage} />
+        <Route path="/pokedex" component={PokedexPage} />
+      </BrowserRouter>
     </div>
   );
 };
